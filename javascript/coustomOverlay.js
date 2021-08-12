2b200d1a320afcbe579535dc96c662579f98e1f4
Ju.D <vavafs136@gmail.com>
2021-08-12 13:05:43 +0900

    []

--- javascript/coustomOverlay.js
+++ javascript/coustomOverlay.js
@@ -16,39 +16,37 @@ var 데이터 = [
             position: markerPosition,
             clickable: true
         })
-        
         marker.setMap(map);
 
         //클릭한 마커의 좌표값을 불러옵니다
         kakao.maps.event.addListener(marker, 'click', function(){
         const position = this.getPosition();
         console.log(position);
-        let i = 0;
+        
+        //데이터 Array에서 인덱스값을 가져옵니다
+        const arrayNumber = 데이터.findIndex(ei => ei.lat === position.Ma);
+
         const storeDetail = document.querySelector(".storeDetail");
-        while (1){
-            var newMarkerPosition  = new kakao.maps.LatLng(데이터[i].lat,데이터[i].lng);
-            console.log(newMarkerPosition);
-            if(position.equals(newMarkerPosition)){
-                console.log(newMarkerPosition);
-                storeDetail.innerHTML ="<iframe class=\"sample\" style=\"width:100%; height: 98%;\"src=\"/pages/ex.html\"></iframe>";
-                
-                var iwContent = 데이터[i].place;
-                var customOverlay = new kakao.maps.CustomOverlay({
-                position: position,
-                content: '<div class ="label"><span class="left"></span><span class="center">' + iwContent + '<button class="button1" onclick="">❌</button></span><span class="right"></span></div><div><a href="ex.html" target="_blank" class="link"></a></div>'
-            })
-            customOverlay.setMap(map);
-            //지도, 마커 클릭 시 커스텀오버레이가 사라집니다
-            kakao.maps.event.addListener(map, 'click', function(){
-            customOverlay.setMap(null);
+        var newMarkerPosition  = new kakao.maps.LatLng(데이터[arrayNumber].lat,데이터[arrayNumber].lng);
+
+        //클릭한 마커값과 인덱스값을 비교합니다
+        if(position.La === newMarkerPosition.La){
+            storeDetail.innerHTML ="<iframe class=\"sample\" style=\"width:100%; height: 98%;\"src=\"/pages/ex"+`${arrayNumber}`+".html\"></iframe>";
+        } else{
             storeDetail.innerHTML ="<iframe class=\"sample\" style=\"width:100%; height: 98%;\"src=\"/pages/sidbar.html\"></iframe>";
-            });
-            break}
-            else {
-                console.log("turn");
-                i++;
-                storeDetail.innerHTML ="<iframe class=\"sample\" style=\"width:100%; height: 98%;\"src=\"/pages/sidbar.html\"></iframe>";    
-            }
-        }
-    });
-}
+        }                
+                
+        var iwContent = 데이터[arrayNumber].place;
+        var customOverlay = new kakao.maps.CustomOverlay({
+        position: position,
+        content: '<div class ="label"><span class="left"></span><span class="center">' + iwContent + '<button class="button1" onclick="">❌</button></span><span class="right"></span></div><div><a href="ex.html" target="_blank" class="link"></a></div>'
+        })
+        customOverlay.setMap(map);
+        
+        //지도, 마커 클릭 시 커스텀오버레이가 사라집니다
+        kakao.maps.event.addListener(map, 'click', function(){
+        customOverlay.setMap(null);
+        storeDetail.innerHTML ="<iframe class=\"sample\" style=\"width:100%; height: 98%;\"src=\"/pages/sidbar.html\"></iframe>";
+        });
+        });
+    };
