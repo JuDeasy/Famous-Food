33af0d0a23d9d2811468e5313cc2ab4b9fa1ea9d
Ju.D <vavafs136@gmail.com>
2021-08-20 17:10:19 +0900

    changes

---
+++ javascript/ui.js
@@ -0,0 +1,10 @@
+const header = document.querySelector("header");
+cons
+
+header.addEventListener("mouseover", function(){
+    console.log("over!");
+}
+)
+header.addEventListener("mouseout", function(){
+    console.log("mouseout!");
+})
